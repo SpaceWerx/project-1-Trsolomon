05238133fdfaab37f4ef4723748aa31a875407e9
Your Name <you@example.com>
2022-05-21 06:55:05 -0400

    Terin 1

--- P1/src/main/java/Models/Menu.java
+++ P1/src/main/java/Models/Menu.java
@@ -1,5 +1,22 @@
 package Models;
 
-public class Menu {
+import java.util.List;
+import java.util.Scanner;
 
+public class Menu {
+	UserServices us = new UserServices;
+	RoleServices rs = new RoleServices;
+	
+	public void displaymenu() {
+		boolean displaymenu = true;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("Welcome to the WE LOVE MONEY Employee Management System");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		
+		
+	}
 }
+	
---
+++ P1/src/main/java/Service/Role_Services.java
@@ -0,0 +1,5 @@
+package Service;
+
+public class Role_Services {
+
+}
