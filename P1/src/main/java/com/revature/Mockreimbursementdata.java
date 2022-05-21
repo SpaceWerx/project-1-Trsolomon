c3211c31718b8c97fdb4ee972b417d477187c2a2
Your Name <you@example.com>
2022-05-21 02:04:33 -0400

    Terin1

--- P1/src/main/java/com/revature/Mockreimbursementdata.java
+++ P1/src/main/java/com/revature/Mockreimbursementdata.java
@@ -1,5 +1,14 @@
 package com.revature;
 
-public class Mockreimbursementdata {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Mockreimbursementdata<Reimbursement> {
+	private final List<Reimbursement> reimbursements = new ArrayList<>();
+public void MockReimbursementData() {
+}
+Mockuserdata userData = new Mockuserdata()
+
+Reimbursement_TO_PROCESS_1 = new Reimbursement(id: 1, author 1, resolver 0, description: "Amazon Web Services", ReimbursementType.other, Status.Pending, amount: 400.00);
+Reimbursement
 }
