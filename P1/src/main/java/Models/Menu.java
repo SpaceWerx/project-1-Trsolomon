c24a46cc5bc4d42a696f78108f8e2fc29ea60dee
Your Name <you@example.com>
2022-05-21 07:15:14 -0400

    Terin 1

--- P1/src/main/java/Models/Menu.java
+++ P1/src/main/java/Models/Menu.java
@@ -3,9 +3,12 @@ package Models;
 import java.util.List;
 import java.util.Scanner;
 
+import Service.Role_Services;
+import Service.User_services;
+
 public class Menu {
-	UserServices us = new UserServices;
-	RoleServices rs = new RoleServices;
+	User_services us = new User_services();
+	Role_Services rs = new Role_Services();
 	
 	public void displaymenu() {
 		boolean displaymenu = true;
@@ -15,6 +18,32 @@ public class Menu {
 		System.out.println("Welcome to the WE LOVE MONEY Employee Management System");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		
+		while(displaymenu) {
+			System.out.println("Hello There -> get greeted");
+			System.out.println("Users -> Show all Users");
+			System.out.println("UsersById -> get Users with a certin ID ");
+			System.out.println("UsersByTitle -> get Users with a certain Title");
+			System.out.println("Add -> add a new User");
+			System.out.println("Exit -> exit the application");
+			
+			String input = scan.nextLine();
+			
+			switch (input) {
+			
+			case "hi": {
+				System.out.println("Hello There");
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				
+				break; 
+				
+				case "employees": {
+				
+			}
+			
+			}
+		}
 		
 		
 	}
