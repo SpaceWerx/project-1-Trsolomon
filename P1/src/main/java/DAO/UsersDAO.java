9c6e9cb0be763b00e8369de192a8097e27842d1d
Your Name <you@example.com>
2022-05-21 04:07:42 -0400

    Terin 1

---
+++ P1/src/main/java/DAO/UsersDAO.java
@@ -0,0 +1,21 @@
+package DAO;
+import java.util.List;
+
+import Models.Users;
+
+public class UsersDAO {
+	public List<Users> getUsers() {
+		return null;
+	}		
+	public void insertUsers(Users newUsers) {
+		
+	}
+	
+	public List<Users> getUsersbyId (int id) {
+		return null;
+	}
+	public List<Users> getUsersbyRole (String role) {
+		return null;
+		
+	}
+}
