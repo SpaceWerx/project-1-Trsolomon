0b79c356da4e85cf256d13e4a99f697f1b2994a1
Your Name <you@example.com>
2022-05-21 02:18:01 -0400

    Terin1

--- P1/src/main/java/com/revature/Mockreimbursementdata.java
+++ P1/src/main/java/com/revature/Mockreimbursementdata.java
@@ -10,5 +10,8 @@ public void MockReimbursementData() {
 Mockuserdata userData = new Mockuserdata()
 
 Reimbursement_TO_PROCESS_1 = new Reimbursement(id: 1, author 1, resolver 0, description: "Amazon Web Services", ReimbursementType.other, Status.Pending, amount: 400.00);
-Reimbursement
+Reimbursement_TO_PROCESS_2 = new Reimbursement(id: 2, author 2, resolver 0, description: "Travel out of state", ReimbursementType.travel, Status.Pending, amount: 550.00);
+Reimbursement_TO_APPROVED_1 = new Reimbursment(id: 3, author 1, resolver 0, description: "Free Dinner for travel purposes", ReimbursementType.food,Status.Approved, amount: 50.00);
+Reimbursement_TO_APPROVED_2 = new Reimbursement(id:4, author 2, resolver 0, description: "3 nights stay at the Houston location", ReimbursementType.lodging, Status.Approved, amount: 450.00);
+Reimbursement_TO_DENIED_1 = new Reimbursement(id:5)
 }
