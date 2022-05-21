3274e1e567ecd594a2a1a3112871fe2b99e3d649
Your Name <you@example.com>
2022-05-21 03:54:46 -0400

    Terin 1

--- P1/src/main/java/Service/User_services.java
+++ P1/src/main/java/Service/User_services.java
@@ -1,5 +1,19 @@
 package Service;
+import java.util.List;
 
+import Models.Users;
 public class User_services {
 
-}
+	UsersDAO uDAO = new UsersDAO();
+	public List<Users> getUsers() {
+		List<Users> users = uDAO.getUsers();
+		return.users;
+		
+		public void Users (Users newUsers) {
+			uDAO.insertUsers(newUsers);
+			
+			
+		}
+
+	}}
+
---
+++ P1/src/main/java/Utilities/Connectionfactory.java
@@ -0,0 +1,13 @@
+package Utilities;
+
+import java.sql.Connection;
+import java.util.Queue;
+
+public class Connectionfactory {
+
+	
+	
+	
+	
+	
+	}
