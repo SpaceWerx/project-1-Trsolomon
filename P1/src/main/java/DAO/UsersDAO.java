d1c46a3bf407154b79d3a97fcd38b5c73e11d7d2
Your Name <you@example.com>
2022-05-21 21:27:18 -0400

    Terin 1

--- P1/src/main/java/DAO/UsersDAO.java
+++ P1/src/main/java/DAO/UsersDAO.java
@@ -13,7 +13,7 @@ public class UsersDAO {
 	
 	public List<Users> getUsers() throws SQLException {
 		try(Connection conn = Connectionfactory.getconnection()){
-		}
+		
 		ResultSet rs = null;
 		return null;
 		
@@ -26,5 +26,22 @@ public class UsersDAO {
 		
 		List<Users> UsersList = new ArrayList<>();
 		
-	}}
+		while(rs.next()) {
+			Users u = new Users();
+					rs.getInt(Users_id);
+					rs.getString(f_name);
+					rs.getString(l_name);
+					rs.getInt(role_id);
+					
+			UsersList.add(u);
+		}
+		return UsersList;{
+	
+		catch(SQLException u) {
+			System.out.println("Something went wrong while grabbing the employees");
+			e.printstacktrace();
+					
+			
+		
+	}}}}
 
