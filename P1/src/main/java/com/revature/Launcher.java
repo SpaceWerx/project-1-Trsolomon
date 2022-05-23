1e07fe22671a888d2eaf325e4f984ca6be76bf08
Your Name <you@example.com>
2022-05-23 14:42:54 -0400

    Terin 1

--- P1/src/main/java/com/revature/Launcher.java
+++ P1/src/main/java/com/revature/Launcher.java
@@ -6,16 +6,18 @@ import java.sql.SQLException;
 
 import org.eclipse.jetty.server.ConnectionFactory;
 
+import Utilities.Connectionfactory;
+
 public class Launcher {
 public static void main(String[] args) throws SQLException {
 	
- try(Connection conn = ConnectionFactory.getConnection()) {
- }	System.out.println("connection successful");
+ try(Connection conn = Connectionfactory.getConnection()) {
+ 	System.out.println("connection successful");
  
 }	catch(SQLException e) {
 	System.out.println("connection failed");
 	e.printStackTrace();
 }
-Menu menu = new Menu();
-menu.displaymenu();
-}
+Menu m = new Menu();
+m.displaymenu();
+}}
